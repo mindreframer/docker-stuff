9d8db086c4371360187ca244a4faef7acf3017fb
Roman Heinrich <roman.heinrich@gmail.com>
2013-06-12 12:39:17 +0200

    Squashed 'github.com/crosbymichael/dockerui/' content from commit addb62f
    git-subtree-dir: github.com/crosbymichael/dockerui
    git-subtree-split: addb62f12824a6a8aa9aa632e54ba5ca00663e47

---
+++ js/filters.js
@@ -0,0 +1,62 @@
+'use strict';
+
+angular.module('dockerui.filters', [])
+    .filter('truncate', function() {
+        return function(text, length, end) {
+            if (isNaN(length))
+                length = 10;
+
+            if (end === undefined)
+                end = "...";
+
+            if (text.length <= length || text.length - end.length <= length) {
+                return text;
+            }
+            else {
+                return String(text).substring(0, length-end.length) + end;
+            }
+        };
+    })
+    .filter('statusbadge', function() {
+        return function(text) {
+            if (text === 'Ghost') {
+                return 'important';
+            } else if (text.indexOf('Exit') != -1 && text !== 'Exit 0') {
+                return 'warning';
+            }
+            return 'success';
+        };
+    })
+    .filter('getstatetext', function() {
+        return function(state) {
+            if (state == undefined) return '';
+
+            if (state.Ghost && state.Running) {
+                return 'Ghost';
+            }
+            if (state.Running) {
+                return 'Running';
+            }
+            return 'Stopped';
+        };
+    })
+    .filter('getstatelabel', function() {
+        return function(state) {
+            if (state == undefined) return '';
+
+            if (state.Ghost && state.Running) {
+                return 'label-important';
+            }
+            if (state.Running) {
+                return 'label-success';
+            }
+            return '';
+        };
+    })
+    .filter('getdate', function() {
+        return function(data) {
+            //Multiply by 1000 for the unix format
+            var date = new Date(data * 1000);
+            return date.toDateString();
+        };    
+    });
