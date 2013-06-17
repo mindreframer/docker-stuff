d095482da0e89b6ad5824d84b666f35c70bbee03
Roman Heinrich <roman.heinrich@gmail.com>
2013-06-17 17:57:05 +0200

    Squashed 'github.com/dotcloud/openstack-docker/' changes from 12e0a8e..c51261d
    c51261d Implemented get_host_stats and get_available_resource in the Nova driver
    69cd9df More Flak8 fixes
    90cadf1 Now passes Flake8 tests
    2e603f7 Merge branch 'master' of github.com:dotcloud/openstack-docker
    639bb26 Implemented Mock Docker client to run unit tests without relying on the Docker daemon client. The driver now passes the Nova virt driver API unit tests.
    
    git-subtree-dir: github.com/dotcloud/openstack-docker
    git-subtree-split: c51261dae0a1c2cd3b03bc94fc68e156268c6d45

--- nova-driver/driver.py
+++ nova-driver/driver.py
@@ -4,21 +4,21 @@
 A Docker Hypervisor which allows running Linux Containers instead of VMs.
 """
 
-import os
-import socket
 import base64
+import os
 import random
+import socket
 import time
 
 from oslo.config import cfg
 
 from nova.compute import power_state
-from nova.compute import task_states
 from nova import exception
-from nova import utils
 from nova.openstack.common import log as logging
-from nova.virt import driver
+from nova import utils
 from nova.virt.docker import client
+from nova.virt.docker import hostinfo
+from nova.virt import driver
 
 CONF = cfg.CONF
 CONF.import_opt('host', 'nova.netconf')
@@ -28,9 +28,9 @@ LOG = logging.getLogger(__name__)
 
 class DockerDriver(driver.ComputeDriver):
     capabilities = {
-        "has_imagecache": True,
-        "supports_recreate": True,
-        }
+        'has_imagecache': True,
+        'supports_recreate': True,
+    }
 
     """Docker hypervisor driver."""
 
@@ -38,6 +38,12 @@ class DockerDriver(driver.ComputeDriver):
         super(DockerDriver, self).__init__(virtapi)
         self.docker = client.HTTPClient()
         self.virtapi = virtapi
+        self.fake = False
+
+    def use_mock_client(self):
+        """Replace HTTP Client with the Mock one (useful for unit tests)."""
+        self.docker = client.MockClient()
+        self.fake = True
 
     def init_host(self, host):
         if self.docker.is_daemon_running() is False:
@@ -47,7 +53,7 @@ class DockerDriver(driver.ComputeDriver):
         res = []
         for container in self.docker.list_containers():
             info = self.docker.inspect_container(container['id'])
-            if _inspect is True:
+            if _inspect:
                 res.append(info)
             else:
                 res.append(info['Config'].get('Hostname'))
@@ -75,35 +81,54 @@ class DockerDriver(driver.ComputeDriver):
         if not container:
             raise exception.InstanceNotFound(instance_id=instance['name'])
         running = container['State'].get('Running')
-        info = {'max_mem': 0,
-                'mem': 0,
-                'num_cpu': 1,
-                'cpu_time': 0}
+        info = {
+            'max_mem': 0,
+            'mem': 0,
+            'num_cpu': 1,
+            'cpu_time': 0
+        }
         info['state'] = power_state.RUNNING if running \
-                else power_state.SHUTDOWN
+            else power_state.SHUTDOWN
         return info
 
     def get_host_stats(self, refresh=False):
+        st = os.statvfs('/var/lib/docker')
         hostname = socket.gethostname()
+        memory = hostinfo.get_memory_usage()
+        disk = hostinfo.get_disk_usage()
         stats = {
-                'hypervisor_hostname': hostname,
-                'host_hostname': hostname,
-                'host_name_label': hostname
-                }
+            'hypervisor_hostname': hostname,
+            'host_hostname': hostname,
+            'host_name_label': hostname,
+            'host_name-description': hostname,
+            'host_memory_total': memory['total'],
+            'host_memory_overhead': memory['used'],
+            'host_memory_free': memory['free'],
+            'host_memory_free_computed': memory['free'],
+            'host_other_config': {},
+            'host_cpu_info': {},
+            'disk_available': disk['available'],
+            'disk_total': disk['total'],
+            'disk_used': disk['used']
+        }
         return stats
 
     def get_available_resource(self, nodename):
-        #TODO: implement
-        return {'vcpus': 1,
-               'memory_mb': 8192,
-               'local_gb': 1028,
-               'vcpus_used': 0,
-               'memory_mb_used': 0,
-               'local_gb_used': 0,
-               'hypervisor_type': 'docker',
-               'hypervisor_version': '1.0',
-               'hypervisor_hostname': nodename,
-               'cpu_info': '?'}
+        memory = hostinfo.get_memory_usage()
+        disk = hostinfo.get_disk_usage()
+        stats = {
+            'vcpus': 1,
+            'memory_mb': memory['total'] / 1024 / 1024,
+            'local_gb': disk['total'] / 1024 / 1024 / 1024,
+            'vcpus_used': 0,
+            'memory_mb_used': memory['used'] / 1024 / 1024,
+            'local_gb_used': disk['used'] / 1024 / 1024 / 1024,
+            'hypervisor_type': 'docker',
+            'hypervisor_version': '1.0',
+            'hypervisor_hostname': nodename,
+            'cpu_info': '?'
+        }
+        return stats
 
     def _find_cgroup_devices_path(self):
         for ln in open('/proc/mounts'):
@@ -125,7 +150,7 @@ class DockerDriver(driver.ComputeDriver):
             return int(pids[0].strip())
 
     def _setup_network(self, instance, network_info):
-        if not network_info:
+        if self.fake is True or not network_info:
             return
         container_id = self.find_container_by_name(instance['name']).get('id')
         if not container_id:
@@ -133,33 +158,40 @@ class DockerDriver(driver.ComputeDriver):
         network_info = network_info[0]
         netns_path = '/var/run/netns'
         if not os.path.exists(netns_path):
-            utils.execute('mkdir', '-p', netns_path,
-                    run_as_root=True)
+            utils.execute(
+                'mkdir', '-p', netns_path, run_as_root=True)
         nspid = self._find_container_pid(container_id)
         if not nspid:
-            raise RuntimeError('Cannot find any PID under '
-                    'container "{0}"'.format(container_id))
+            raise RuntimeError(
+                'Cannot find any PID under '
+                'container "{0}"'.format(container_id))
         netns_path = os.path.join(netns_path, container_id)
-        utils.execute('ln', '-sf', '/proc/{0}/ns/net'.format(nspid),
-                '/var/run/netns/{0}'.format(container_id),
-                run_as_root=True)
+        utils.execute(
+            'ln', '-sf', '/proc/{0}/ns/net'.format(nspid),
+            '/var/run/netns/{0}'.format(container_id),
+            run_as_root=True)
         rand = random.randint(0, 100000)
         if_local_name = 'pvnetl{0}'.format(rand)
         if_remote_name = 'pvnetr{0}'.format(rand)
         bridge = network_info[0]['bridge']
         ip = network_info[1]['ips'][0]['ip']
-        utils.execute('ip', 'link', 'add', 'name', if_local_name, 'type',
-                'veth', 'peer', 'name', if_remote_name,
-                run_as_root=True)
-        utils.execute('brctl', 'addif', bridge, if_local_name,
-                run_as_root=True)
-        utils.execute('ip', 'link', 'set', if_local_name, 'up',
-                run_as_root=True)
-        utils.execute('ip', 'link', 'set', if_remote_name, 'netns', nspid,
-                run_as_root=True)
-        utils.execute('ip', 'netns', 'exec', container_id, 'ifconfig',
-                if_remote_name, ip,
-                run_as_root=True)
+        utils.execute(
+            'ip', 'link', 'add', 'name', if_local_name, 'type',
+            'veth', 'peer', 'name', if_remote_name,
+            run_as_root=True)
+        utils.execute(
+            'brctl', 'addif', bridge, if_local_name,
+            run_as_root=True)
+        utils.execute(
+            'ip', 'link', 'set', if_local_name, 'up',
+            run_as_root=True)
+        utils.execute(
+            'ip', 'link', 'set', if_remote_name, 'netns', nspid,
+            run_as_root=True)
+        utils.execute(
+            'ip', 'netns', 'exec', container_id, 'ifconfig',
+            if_remote_name, ip,
+            run_as_root=True)
 
     def spawn(self, context, instance, image_meta, injected_files,
               admin_password, network_info=None, block_device_info=None):
@@ -169,10 +201,10 @@ class DockerDriver(driver.ComputeDriver):
             cmd = ['/bin/sh', '-c', base64.b64decode(user_data)]
         image_name = image_meta.get('name', 'ubuntu')
         args = {
-                'Hostname': instance['name'],
-                'Image': image_name,
-                'Cmd': cmd
-                }
+            'Hostname': instance['name'],
+            'Image': image_name,
+            'Cmd': cmd
+        }
         container_id = self.docker.create_container(args)
         if container_id is None:
             LOG.info('Image name "{0}" does not exist, fetching it...'.format(
@@ -180,20 +212,20 @@ class DockerDriver(driver.ComputeDriver):
             res = self.docker.pull_repository(image_name)
             if res is False:
                 raise exception.InstanceDeployFailure(
-                        'Cannot pull missing image',
-                        instance_id=instance['name'])
+                    'Cannot pull missing image',
+                    instance_id=instance['name'])
             container_id = self.docker.create_container(args)
             if container_id is None:
                 raise exception.InstanceDeployFailure(
-                        'Cannot create container',
-                        instance_id=instance['name'])
+                    'Cannot create container',
+                    instance_id=instance['name'])
         self.docker.start_container(container_id)
         try:
             self._setup_network(instance, network_info)
         except Exception as e:
             raise exception.InstanceDeployFailure(
-                    'Cannot setup network: {0}'.format(e),
-                    instance_id=instance['name'])
+                'Cannot setup network: {0}'.format(e),
+                instance_id=instance['name'])
 
     def destroy(self, instance, network_info, block_device_info=None,
                 destroy_disks=True):
@@ -228,10 +260,3 @@ class DockerDriver(driver.ComputeDriver):
         if not container_id:
             return
         return self.docker.get_container_logs(container_id)
-
-
-#TEST
-def _dump(var):
-    import sys, json
-    print json.dumps(var, indent=4)
-    sys.exit(1)
