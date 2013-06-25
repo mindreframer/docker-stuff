c71f487b58d565a5111af50c8db8e9bb167fac56
Roman Heinrich <roman.heinrich@gmail.com>
2013-06-26 00:43:19 +0200

    Squashed 'github.com/globocom/docker-cluster/' changes from 7239ecd..dcfe616
    dcfe616 fixed error on commit.
    02467b6 cluster/image: add tests for failure in (Pull|Push)Image
    9480e88 cluster: fix bug in runOnNodes
    b7f62af cluster/image: add PushImage
    00fd46d cluster/image: add PullImage
    60937c1 contributors: readd banner explaining the file
    6699f08 cluster: add test to make sure that RemoveImage returns proper error
    7aa2510 cluster/image: add RemoveImage
    e6127a2 cluster: generalize and move runOnNodes
    
    git-subtree-dir: github.com/globocom/docker-cluster
    git-subtree-split: dcfe616b5d5d9dec048a0c901784926d1b0af930

---
+++ cluster/image.go
@@ -0,0 +1,37 @@
+// Copyright 2013 docker-cluster authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package cluster
+
+import (
+	"github.com/fsouza/go-dockerclient"
+	"io"
+)
+
+// RemoveImage removes an image from all nodes in the cluster, returning an
+// error in case of failure.
+func (c *Cluster) RemoveImage(name string) error {
+	_, err := c.runOnNodes(func(n node) (interface{}, error) {
+		return nil, n.RemoveImage(name)
+	}, docker.ErrNoSuchImage)
+	return err
+}
+
+// PullImage pulls an image from a remote registry server, returning an error
+// in case of failure.
+func (c *Cluster) PullImage(opts docker.PullImageOptions, w io.Writer) error {
+	_, err := c.runOnNodes(func(n node) (interface{}, error) {
+		return nil, n.PullImage(opts, w)
+	}, docker.ErrNoSuchImage)
+	return err
+}
+
+// PushImage pushes an image to a remote registry server, returning an error in
+// case of failure.
+func (c *Cluster) PushImage(opts docker.PushImageOptions, w io.Writer) error {
+	_, err := c.runOnNodes(func(n node) (interface{}, error) {
+		return nil, n.PushImage(opts, w)
+	}, docker.ErrNoSuchImage)
+	return err
+}
