1b720d136e76d05fecb1f0646429bdfb119469ce
Roman Heinrich <roman.heinrich@gmail.com>
2013-06-27 11:33:06 +0200

    https://github.com/ptone/jiffylab.git

--- github.com/crosbymichael/dockerui/js/app.js
+++ github.com/crosbymichael/dockerui/js/app.js
@@ -12,7 +12,7 @@ angular.module('dockerui', ['dockerui.services', 'dockerui.filters'])
     }])
     // This is your docker url that the api will use to make requests
     // You need to set this to the api endpoint without the port i.e. http://192.168.1.9
-    .constant('DOCKER_ENDPOINT', '/dockerapi')
+    .constant('DOCKER_ENDPOINT', 'http://localhost:4243/dockerapi')
     .constant('DOCKER_PORT', '') // Docker port, leave as an empty string if no port is requred.  If you have a port, prefix it with a ':' i.e. :4243
     .constant('UI_VERSION', 'v0.2')
     .constant('DOCKER_API_VERSION', 'v1.2');
