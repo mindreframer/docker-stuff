683a8ec93bf30e15eef85e60b431f66bf5dbecb1
Roman Heinrich <roman.heinrich@gmail.com>
2013-07-14 21:39:20 +0200

    Squashed 'github.com/progrium/dokku/' changes from b15b55f..b433626
    b433626 use docker 0.4.8 deb, but 0.4.2 binary. sigh
    3d1c8a3 Merge pull request #84 from hwartig/update-docker
    3aa6340 Update docker to latest version 0.4.8-1 .
    ea14fb0 Merge pull request #77 from asm89/bootstrap
    264d196 Merge remote-tracking branch 'origin/master' into bootstrap
    1f45937 Merge pull request #71 from ekristen/master
    c118c20 Merge pull request #75 from interstateone/wget-filename
    5493236 Merge pull request #79 from ema/master
    bc85239 Point to the right conf file for domain name
    49f1c44 cde -> cd
    621de7c We don't want the progrium version here ;)
    99dcacd Tune "Advanced installation" documentation
    713365f Make it possible to override dependency urls
    5065810 Move copying of plugins to install target
    eaa486e Separate plugin-install command
    04e0618 Just use the non-redirected URL instead
    30bd933 Change filename used in Advanced Installation section of README
    6795843 Merge pull request #74 from asm89/patch-4
    e08aac8 Fix typo
    65f1dd7 Merge pull request #73 from asm89/patch-3
    a3cf19b Add progrium/pluginhook to README
    a0eb48c ensure that the original http_host gets passed in the proxy to the app
    bc249ef lets avoid pointless redundancies
    2dfe1e1 refactoring most of bootstrap into makefile. bootstrap supports branch checkouts, and using existing cloned dokku repos
    19a0ee2 Merge pull request #70 from rhy-jot/master
    e8e6267 Fix placement of my username
    ee80167 Merge pull request #69 from asm89/authors
    bcd3ba3 Add me to authors and sort
    5e08d92 Merge pull request #68 from asm89/vagrant-test-setup
    5653eed Fix vagrant test setup
    407e3f0 Merge pull request #65 from asm89/makefile
    6d8232d Merge pull request #62 from asm89/patch-1
    9623b4c Merge pull request #64 from asm89/patch-2
    47ca5bd Merge pull request #67 from asm89/pre-deploy-hook
    cf7a2a0 Add pre-deploy hook
    1024492 Create separate copy target in Makefile
    4f07b7a Escape variable in nginx post-deploy script
    e75099d Run apt-get update after adding ppa
    0b07e4e more specific line counts
    4ecb13a disabling websocket test since its currently broken
    1626b10 Merge pull request #60 from grigio/master
    7b9d180 me in AUTHORS
    538925f Merge pull request #59 from grigio/master
    4d77b66 websocket test init
    de906a2 websocket support in NGINX , see: #45
    60f29cd adding some basic help, also as another example of using plugin hooks
    997d2dd only override url if vhost file exists
    071c446 Merge branch 'dev'
    5c0f732 doing it the mike-park way
    c4fd6d6 nginx vhosts as a plugin?
    51dc343 make sure to not fail
    79b099e moving install hook trigger into make install
    6be113f using absolute path for plugins
    1ebfc94 forgot how to use wget
    17953fc initial attempt at plugins
    98859df only run travis for master
    c08f323 rolled into dokku
    183d8f6 ppa still 0.4.6, so no luck testing 0.4.7 easily either
    caa03d0 forgot to offest args vars
    6d2954d nope, no luck for testing branches
    3ce4897 hopefully get travis to test this particular branch
    1ee35cb fairly major refactoring. consolidating code into dokku script. also, went back to installing docker from ppa
    0d49424 source based roadmap file dropped in favor of issue tracked roadmaps. for example: https://github.com/progrium/dokku/issues/55
    7ecdba7 poor man's retry
    fc39649 exiting with 7 means could not connect. adding sleep to give container a second to spin up
    65fb3c1 irc notifications
    977d126 excellent CI system catching my mistakes
    70be1da removing buildstep submodule and replacing it with the buildstep script that dokku actually uses. the buildstep project is otherwise completely separate other than the artifact it builds (the progrium/buildstep container)
    dc2c482 Update README.md
    d5e264d Merge pull request #50 from dgryski/master
    0d6d7cd tests: reenable go test
    601bbc2 tests: "something" doesn't like the app just being called 'go'
    
    git-subtree-dir: github.com/progrium/dokku
    git-subtree-split: b433626b4e1f119f1bf986c177a12a6ed0c436d3

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,33 +1,13 @@
-DOKKU_REPO=${DOKKU_REPO:-"https://github.com/progrium/dokku.git"}
-DOKKU_STACK=${DOKKU_STACK:-"https://s3.amazonaws.com/progrium-dokku/progrium_buildstep.tgz"}
-DOCKER_PKG=${DOCKER_PKG:-"https://launchpad.net/~dotcloud/+archive/lxc-docker/+files/lxc-docker_0.4.2-1_amd64.deb"}
 set -e
+export DEBIAN_FRONTEND=noninteractive
+export DOKKU_REPO=${DOKKU_REPO:-"https://github.com/progrium/dokku.git"}
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y linux-image-extra-`uname -r`
-apt-get install -y git nginx make curl dnsutils
+apt-get install -y git make curl
 
-wget -qO- "$DOCKER_PKG" > /tmp/lxc-docker_0.4.2-1_amd64.deb
-dpkg --force-depends -i /tmp/lxc-docker_0.4.2-1_amd64.deb && apt-get install -f -y
-rm /tmp/lxc-docker_0.4.2-1_amd64.deb
-
-sed -i.bak 's/docker -d/docker -d -r=true/' /etc/init/docker.conf # Docker should restart containers 
-
-cd ~ && git clone ${DOKKU_REPO}
-cd dokku && make install
-if [[ $DOKKU_STACK ]]; then
-  curl "$DOKKU_STACK" | gunzip -cd | docker import - progrium/buildstep
-else
-  cd buildstep && make build
-fi
-
-[[ -f /etc/nginx/nginx.conf ]] && sed -i 's/# server_names_hash_bucket_size/server_names_hash_bucket_size/' /etc/nginx/nginx.conf
-
-/etc/init.d/nginx start
-start nginx-reloader
-
-[[ $(dig +short $HOSTNAME) ]] && HOSTFILE=DOMAIN || HOSTFILE=HOSTNAME
-echo $HOSTNAME > /home/git/$HOSTFILE
+cd ~ && test -d dokku || git clone $DOKKU_REPO
+cd dokku && test $DOKKU_BRANCH && git checkout origin/$DOKKU_BRANCH || true
+make all
 
 echo
 echo "Be sure to upload a public key for your user:"
