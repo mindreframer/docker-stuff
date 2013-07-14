9483e1f51f551ce671cf2fce6eae5972da95ca8e
Roman Heinrich <roman.heinrich@gmail.com>
2013-07-14 21:37:42 +0200

    Squashed 'github.com/rainforestapp/Docker-Terminal/' content from commit ea2dd9b
    git-subtree-dir: github.com/rainforestapp/Docker-Terminal
    git-subtree-split: ea2dd9b400e88666ec9765383d9330a51110e429

---
+++ docker_terminal.js
@@ -0,0 +1,49 @@
+window.docker = (function(docker) {
+  docker.terminal = {
+    startTerminalForContainer: function(host, container) {
+      var term = new Terminal();
+      term.open();
+
+      var wsUri = "ws://" + 
+        host + 
+        "/v1.3/containers/" + 
+        container + 
+        "/attach/ws?logs=1&stderr=1&stdout=1&stream=1&stdin=1";
+
+      var websocket = new WebSocket(wsUri);
+      websocket.onopen = function(evt) { onOpen(evt) };
+      websocket.onclose = function(evt) { onClose(evt) };
+      websocket.onmessage = function(evt) { onMessage(evt) };
+      websocket.onerror = function(evt) { onError(evt) };
+
+      term.on('data', function(data) {
+        websocket.send(data);
+      });
+
+      function onOpen(evt) { 
+        term.write("Session started");
+      }  
+
+      function onClose(evt) { 
+        term.write("Session terminated");
+      }  
+
+      function onMessage(evt) { 
+        term.write(evt.data);
+      }  
+
+      function onError(evt) { 
+      }  
+    }
+  };
+
+  return docker;
+})(window.docker || {});
+
+$(function() {
+  $("[data-docker-terminal-container]").each(function(i, el) {
+    var container = $(el).data('docker-terminal-container');
+    var host = $(el).data('docker-terminal-host');
+    docker.terminal.startTerminalForContainer(host, container);
+  });
+});
