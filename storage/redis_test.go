e2095da90f6d3ad836de82e35b64c9714a9246b2
Roman Heinrich <roman.heinrich@gmail.com>
2013-07-23 17:32:33 +0200

    Squashed 'github.com/globocom/docker-cluster/' changes from 62c0153..57fe180
    57fe180 cluster: call Storage.Remove on RemoveContainer
    7b0b45b storage: add edge case test
    dfde968 storage: fix bug in Retrieve when container does not exist
    591e53d storage: include Remove calls in the "integration" tests
    54c98d8 storage: implement redisStorage.Remove
    401264f cluster: add Storage.Remove
    342b2bc storage: add functions to get the storage instance
    825fb57 storage: fix code
    109ccf4 storage/tests: add type to start redis server on demmand
    1e62124 travis: run tests from all packages
    785febb storage: implement Retrieve in the redis storage
    b9aaf4e storage: starting package
    
    git-subtree-dir: github.com/globocom/docker-cluster
    git-subtree-split: 57fe180c6a7838541f4f1cee305a1866ae765ab7

---
+++ storage/redis_test.go
@@ -0,0 +1,255 @@
+// Copyright 2013 docker-cluster authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package storage
+
+import (
+	"github.com/garyburd/redigo/redis"
+	"reflect"
+	"testing"
+)
+
+func TestRedisStorageStore(t *testing.T) {
+	conn := fakeConn{}
+	var storage redisStorage
+	storage.pool = redis.NewPool(func() (redis.Conn, error) {
+		return &conn, nil
+	}, 3)
+	container := "affe3022"
+	host := "server0"
+	err := storage.Store(container, host)
+	if err != nil {
+		t.Error(err)
+	}
+	cmd := conn.cmds[0]
+	expectedCmd := "SET"
+	if cmd.cmd != expectedCmd {
+		t.Errorf("Store(%q, %q): want command %q. Got %q.", container, host, expectedCmd, cmd.cmd)
+	}
+	expectedArgs := []interface{}{container, host}
+	if !reflect.DeepEqual(cmd.args, expectedArgs) {
+		t.Errorf("Store(%q, %q): want args %#v. Got %#v.", container, host, expectedArgs, cmd.args)
+	}
+}
+
+func TestRedisStorageStoreFailure(t *testing.T) {
+	conn := failingFakeConn{}
+	var storage redisStorage
+	storage.pool = redis.NewPool(func() (redis.Conn, error) {
+		return &conn, nil
+	}, 3)
+	container := "affe3022"
+	host := "server0"
+	err := storage.Store(container, host)
+	if err == nil {
+		t.Error("Got unexpected <nil> error")
+	}
+}
+
+func TestRedisStorageRetrieve(t *testing.T) {
+	conn := resultCommandConn{
+		fakeConn: &fakeConn{},
+		reply:    map[string]interface{}{"GET": []byte("server0")},
+	}
+	var storage redisStorage
+	storage.pool = redis.NewPool(func() (redis.Conn, error) {
+		return &conn, nil
+	}, 3)
+	container := "affe3022"
+	host, err := storage.Retrieve(container)
+	if err != nil {
+		t.Error(err)
+	}
+	expectedHost := "server0"
+	if host != expectedHost {
+		t.Errorf("Retrieve(%q): want host %q. Got %q.", container, expectedHost, host)
+	}
+	cmd := conn.cmds[0]
+	expectedCmd := "GET"
+	if cmd.cmd != expectedCmd {
+		t.Errorf("Retrieve(%q): want command %q. Got %q.", container, expectedCmd, cmd.cmd)
+	}
+	expectedArgs := []interface{}{container}
+	if !reflect.DeepEqual(cmd.args, expectedArgs) {
+		t.Errorf("Retrieve(%q): want args %#v. Got %#v.", container, expectedArgs, cmd.args)
+	}
+}
+
+func TestRedisStorageRetrieveFailure(t *testing.T) {
+	conn := failingFakeConn{}
+	var storage redisStorage
+	storage.pool = redis.NewPool(func() (redis.Conn, error) {
+		return &conn, nil
+	}, 3)
+	container := "affe3022"
+	_, err := storage.Retrieve(container)
+	if err == nil {
+		t.Errorf("Retrieve(%q): Got unexpected <nil> error", container)
+	}
+}
+
+func TestRedisStorageRetrieveNoSuchContainer(t *testing.T) {
+	conn := resultCommandConn{
+		fakeConn: &fakeConn{},
+		reply:    map[string]interface{}{"GET": nil},
+	}
+	var storage redisStorage
+	storage.pool = redis.NewPool(func() (redis.Conn, error) {
+		return &conn, nil
+	}, 3)
+	container := "affe3022"
+	_, err := storage.Retrieve(container)
+	if err != ErrNoSuchContainer {
+		t.Errorf("Retrieve(%q): wrong error. Want %#v. Got %#v.", container, ErrNoSuchContainer, err)
+	}
+}
+
+func TestRedisStorageRemove(t *testing.T) {
+	conn := resultCommandConn{
+		fakeConn: &fakeConn{},
+		reply:    map[string]interface{}{"DEL": int64(1)},
+	}
+	var storage redisStorage
+	storage.pool = redis.NewPool(func() (redis.Conn, error) {
+		return &conn, nil
+	}, 3)
+	container := "affe3022"
+	err := storage.Remove(container)
+	if err != nil {
+		t.Error(err)
+	}
+	cmd := conn.cmds[0]
+	expectedCmd := "DEL"
+	if cmd.cmd != expectedCmd {
+		t.Errorf("Remove(%q): want command %q. Got %q.", container, expectedCmd, cmd.cmd)
+	}
+	expectedArgs := []interface{}{container}
+	if !reflect.DeepEqual(cmd.args, expectedArgs) {
+		t.Errorf("Remove(%q): want args %#v. Got %#v.", container, expectedArgs, cmd.args)
+	}
+}
+
+func TestRedisRemoveFailure(t *testing.T) {
+	var conn failingFakeConn
+	var storage redisStorage
+	storage.pool = redis.NewPool(func() (redis.Conn, error) {
+		return &conn, nil
+	}, 3)
+	err := storage.Remove("affe3022")
+	if err == nil {
+		t.Error("Unexpected <nil> error")
+	}
+}
+
+func TestRedisRemoveNoSuchContainer(t *testing.T) {
+	conn := resultCommandConn{
+		fakeConn: &fakeConn{},
+		reply:    map[string]interface{}{"DEL": int64(0)},
+	}
+	var storage redisStorage
+	storage.pool = redis.NewPool(func() (redis.Conn, error) {
+		return &conn, nil
+	}, 3)
+	container := "affe3022"
+	err := storage.Remove(container)
+	if err != ErrNoSuchContainer {
+		t.Errorf("Remove(%q): wrong error. Want %#v. Got %#v.", container, ErrNoSuchContainer, err)
+	}
+}
+
+func TestRedisNoAuthentication(t *testing.T) {
+	var server redisServer
+	err := server.start()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.stop()
+	storage := Redis(server.addr())
+	container := "affe3022"
+	host := "server0"
+	_, err = storage.Retrieve(container)
+	if err != ErrNoSuchContainer {
+		t.Errorf("Retrieve(%q): wrong error. Want %#v. Got %#v", container, ErrNoSuchContainer, err)
+	}
+	err = storage.Remove(container)
+	if err != ErrNoSuchContainer {
+		t.Errorf("Remove(%q): wrong error. Want %#v. Got %#v", container, ErrNoSuchContainer, err)
+	}
+	err = storage.Store(container, host)
+	if err != nil {
+		t.Error(err)
+	}
+	gotHost, err := storage.Retrieve(container)
+	if err != nil {
+		t.Error(err)
+	}
+	if gotHost != host {
+		t.Errorf("Store and Retrieve returned wrong value. Want %q. Got %q.", host, gotHost)
+	}
+	err = storage.Remove(container)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestRedisStorageConnectionFailure(t *testing.T) {
+	storage := Redis("something_unknown:39494")
+	err := storage.Store("affe3022", "server0")
+	if err == nil {
+		t.Error("Got unexpected <nil> error")
+	}
+}
+
+func TestRedisStorageAuthentication(t *testing.T) {
+	var server redisServer
+	server.password = "123456"
+	err := server.start()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.stop()
+	storage := AuthenticatedRedis(server.addr(), "123456")
+	container := "affe3022"
+	host := "server0"
+	_, err = storage.Retrieve(container)
+	if err != ErrNoSuchContainer {
+		t.Errorf("Retrieve(%q): wrong error. Want %#v. Got %#v", container, ErrNoSuchContainer, err)
+	}
+	err = storage.Remove(container)
+	if err != ErrNoSuchContainer {
+		t.Errorf("Remove(%q): wrong error. Want %#v. Got %#v", container, ErrNoSuchContainer, err)
+	}
+	err = storage.Store(container, host)
+	if err != nil {
+		t.Error(err)
+	}
+	gotHost, err := storage.Retrieve(container)
+	if err != nil {
+		t.Error(err)
+	}
+	if gotHost != host {
+		t.Errorf("Store and Retrieve returned wrong value. Want %q. Got %q.", host, gotHost)
+	}
+	err = storage.Remove(container)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestRedisStorageAuthenticationFailure(t *testing.T) {
+	var server redisServer
+	server.password = "123456"
+	err := server.start()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.stop()
+	storage := AuthenticatedRedis(server.addr(), "123")
+	container := "affe3022"
+	host := "server0"
+	err = storage.Store(container, host)
+	if err == nil {
+		t.Error("Got unexpected <nil> error")
+	}
+}
