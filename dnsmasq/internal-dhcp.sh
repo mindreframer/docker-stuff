cf4539efd0615ad4e44ff206b6592ee859daa5c5
Roman Heinrich <roman.heinrich@gmail.com>
2013-09-10 08:51:18 +0200

    Squashed 'github.com/cvlc/orchestrator/' content from commit 1b3e7b6
    git-subtree-dir: github.com/cvlc/orchestrator
    git-subtree-split: 1b3e7b687b3599c5c1bb4ca775316712c96a359e

---
+++ dnsmasq/internal-dhcp.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# These should be adjusted to reflect the plugin helper's settings.cfg.
+DIR="/home/$USER/orchestrator/dnsmasq"
+HOST="localhost"
+PORT="8998"
+CHANGED="0"
+ADDRESS="fd39:9706:2786:6333::1"
+SERVER_ID=$(/usr/bin/curl -s -k "https://$HOST:$PORT/cloud/$ADDRESS")
+
+if [[ "$SERVER_ID" == 'false' ]]; then
+    SERVER_ID=$(/usr/bin/curl -s -k --data "service_address=$ADDRESS" "https://$HOST:$PORT/cloud/add")
+    echo "Server ID: $SERVER_ID"
+fi
+
+if [[ "$1" == "add" ]]; then
+    if [[ "$DNSMASQ_IAID" ]]; then
+        echo "$DNSMASQ_IAID found, creating image"
+        ADDINSTANCE=$(/usr/bin/curl -s -k --data "server_id=$SERVER_ID&instance_address=$3&instance_duid=$2" "https://$HOST:$PORT/instance/add")
+        echo $ADDINSTANCE
+        EXISTS=$(grep "$2" "$DIR/int-staticaddr")
+        if [[ "$EXISTS" == '' ]]; then
+	    if [ ! -e $DIR/int-staticaddr ]; then touch 
+$DIR/int-staticaddr; 
+fi
+            echo "id:$2,[$3]" >> "$DIR/int-staticaddr"
+            CHANGED="1"
+        fi
+    fi
+fi
+
+if [[ "$1" == "old" ]]; then
+    if [[ "$DNSMASQ_IAID" ]]; then
+        echo "$DNSMASQ_IAID found, old"
+        OLDINSTANCE=$(/usr/bin/curl -s -k "https://$HOST:$PORT/instance/up" 2>/dev/null)
+        echo $OLDCLIENT
+    fi
+fi
+
+if [[ $1 == "del" ]]; then
+    if [[ "$DNSMASQ_IAID" ]]; then
+        echo "$DNSMASQ_IAID found, del"
+        DELINSTANCE=$(/usr/bin/curl -s -k "--data \"instance_address=$3\"" "https://$HOST:$PORT/instance/rm" 2>/dev/null)
+        echo $DELINSTANCE
+        EXISTS=$(grep --quiet "$2" "$DIR/int-staticaddr")
+        if $EXISTS; then
+	    if [ ! -e $DIR/int-staticaddr ]; then touch 
+$DIR/int-staticaddr; fi
+            sed -i "/$3/d" "$DIR/int-staticaddr"
+            CHANGED="1"
+        fi
+    fi
+fi
+
+if [[ "$CHANGED" == "1" ]]; then
+/usr/bin/ps aux | awk '/[d]nsmasq-internal/{print $2}' | xargs kill -HUP
+fi
