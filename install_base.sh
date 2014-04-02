e3fd6b7d97b6436704761cbed69a16e2c9e57ba9
Roman Heinrich <roman.heinrich@gmail.com>
2014-04-02 20:29:32 +0200

    Squashed 'github.com/henrik-muehe/docker-ssh/' content from commit 1d6eeba
    git-subtree-dir: github.com/henrik-muehe/docker-ssh
    git-subtree-split: 1d6eebabce52fae6f62eab3328343e1908a059f6

---
+++ install_base.sh
@@ -0,0 +1,48 @@
+TMPDIR="/tmp/docker-ssh-l3jk2413"
+
+# Search for nc dependency
+which uudecode >/dev/null 2>/dev/null
+if [ "$?" != "0" ]; then
+	echo "Please install uudecode first."
+	exit 1
+fi
+
+# after http://www.linuxjournal.com/content/add-binary-payload-your-shell-scripts
+function extract_payload()
+{
+	rm -rf $TMPDIR
+	mkdir -p $TMPDIR
+	echo "$PAYLOAD" | uudecode | tar -xzf - -C $TMPDIR
+}
+
+function cleanup()
+{
+	rm -rf $TMPDIR
+}
+
+# create directory and restrict permissions if it doesn't exist
+mkdir ~/.ssh 2>/dev/null && chmod 700 ~/.ssh
+
+# extract data
+extract_payload
+
+# check for previous install of this addon
+touch ~/.ssh/config
+if grep -q "\\.docker" ~/.ssh/config || grep -q "docker-proxy" ~/.ssh/config; then
+	echo "Upgrading docker-ssh"
+else
+	echo "Installing docker-ssh"
+
+	# install config
+	cat $TMPDIR"/config" >> ~/.ssh/config
+	chmod 600 ~/.ssh/config
+fi
+
+# upgrade/install proxy
+cp $TMPDIR"/docker-proxy" ~/.ssh/
+chmod 700 ~/.ssh/docker-proxy
+
+# cleanup
+cleanup
+
+exit 0
